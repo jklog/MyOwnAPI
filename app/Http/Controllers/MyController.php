60ed864007d21e7667c624c7be5418206bd6ab57
jklog <jklog@att.net>
2015-04-27 11:20:45 -0700

    First Commit
    - Uploading all the new Laravel 5 structure.

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,9 @@
+<?php namespace App\Http\Controllers;
+
+class MyController extends Controller
+{
+  public function index($name = '"My Default User Name"')
+  {
+    return view('hi', ['name' => $name]);
+  }
+}
---
+++ resources/views/hi.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+  <title>Hi Document</title>
+</head>
+<body>
+    <h1>Hello {{ $name }}</h1>
+</body>
+</html>
